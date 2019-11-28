62b10243ac1d4a130cbf636d602079ae0261bdb0
Pradeep44 <pradeepmudvari066@gmail.com>
2019-11-28 11:31:57 +0545

    app initialize

---
+++ src/components/Booklist.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class BookList extends Component {
+    render() { 
+        return ( 
+            <div className='book-list'>
+                <ul>
+                    <li>demo 1</li>
+                    <li>demo 2</li>
+                    <li>demo 3</li>
+                </ul>
+            </div>
+         );
+    }
+}
+ 
+export default BookList;
